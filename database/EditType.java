818513b28e989fd5bbb543832bda309410611e9d
LeoHLee <48520136+LeoHLee@users.noreply.github.com>
2020-06-07 01:58:30 +0800

    Add files via upload

---
+++ database/EditType.java
@@ -0,0 +1,19 @@
+package database;
+/**
+ * 修改信息类型 
+ * 		密码--Password
+ * 		密保邮箱--MailAddress
+ * 		用户昵称--NickName
+ * 		生日--Birthday
+ * 		性别--Sex
+ * 		头像--Avatar
+ * @author   LightHouse
+ */
+public enum EditType {
+	Password,
+	MailAddress,
+	NickName,
+	Birthday,
+	Sex,
+	Avatar
+}
